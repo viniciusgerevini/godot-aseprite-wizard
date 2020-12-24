8a339f4820f1c046ee2e8fa96a10d8aa8adb76f9
Vinicius Gerevini <viniciusgerevini@gmail.com>
2020-12-24 12:14:09 +1100

    refactored config dialog script

--- addons/AsepriteWizard/config_dialog.gd
+++ addons/AsepriteWizard/config_dialog.gd
@@ -1,30 +1,30 @@
 tool
 extends WindowDialog
 
-var config: ConfigFile
-
 signal importer_state_changed
 
-func _ready():
-	if config.has_section_key('aseprite', 'command'):
-		_aseprite_command_field().text = config.get_value('aseprite', 'command')
-	else:
-		_aseprite_command_field().text = get_default_command()
+const _CONFIG_SECTION_KEY = 'aseprite'
+const _COMMAND_KEY = 'command'
+const _IMPORTER_ENABLE_KEY = 'is_importer_enabled'
 
-	if config.has_section_key('aseprite', 'is_importer_enabled'):
-		_importer_enable_field().pressed = config.get_value('aseprite', 'is_importer_enabled')
+var config: ConfigFile
+
+
+func _ready():
+	_aseprite_command_field().text = config.get_value(_CONFIG_SECTION_KEY, _COMMAND_KEY, get_default_command())
+	_importer_enable_field().pressed = config.get_value(_CONFIG_SECTION_KEY, _IMPORTER_ENABLE_KEY, true)
 
 func init(config_file: ConfigFile):
 	config = config_file
 
 func _on_save_button_up():
 	if _aseprite_command_field().text == "":
-		config.set_value('aseprite', 'command', get_default_command())
+		config.set_value(_CONFIG_SECTION_KEY, _COMMAND_KEY, get_default_command())
 	else:
-		config.set_value('aseprite', 'command', _aseprite_command_field().text)
+		config.set_value(_CONFIG_SECTION_KEY, _COMMAND_KEY, _aseprite_command_field().text)
 
-	if _importer_enable_field().pressed != config.get_value('aseprite', 'is_importer_enabled', true):
-		config.set_value('aseprite', 'is_importer_enabled', _importer_enable_field().pressed)
+	if _importer_enable_field().pressed != config.get_value(_CONFIG_SECTION_KEY, _IMPORTER_ENABLE_KEY, true):
+		config.set_value(_CONFIG_SECTION_KEY, _IMPORTER_ENABLE_KEY, _importer_enable_field().pressed)
 		self.emit_signal("importer_state_changed")
 
 	self.hide()
