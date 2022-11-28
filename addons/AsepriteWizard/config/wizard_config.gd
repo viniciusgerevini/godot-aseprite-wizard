d6201d1c6c3dbea5f5b57400240e8f8f602a61bd
Daniel Keenan <dfkeenan@gmail.com>
2022-11-28 23:46:02 +1000

    Store config in metadata (#63)
    * Change config decode to convert bool types.
    
    * Change import to save config as metadata
    
    * Add option to toggle storing config in metadata.
    
    * Added metadata_export_plugin and config.
    
    * Changed metadata export setting location and name.

--- addons/AsepriteWizard/config/wizard_config.gd
+++ addons/AsepriteWizard/config/wizard_config.gd
@@ -1,6 +1,7 @@
 tool
 extends Reference
 
+const WIZARD_CONFIG_META_NAME = "_aseprite_wizard_config_"
 const WIZARD_CONFIG_MARKER = "aseprite_wizard_config"
 const SEPARATOR = "|="
 
@@ -23,7 +24,21 @@ static func decode(string: String):
 	for c in cfg:
 		var parts = c.split(SEPARATOR, 1)
 		if parts.size() == 2:
-			config[parts[0].strip_edges()] = parts[1].strip_edges()
+			var key = parts[0].strip_edges()
+			var value = parts[1].strip_edges()
+			
+			#Convert bool properties
+			if key == "only_visible" or key == "op_exp":
+				match value:
+					"True":
+						config[key] = true
+					"False":
+						config[key] = false
+					_:
+						config[key] = false
+			else:
+				config[key] = value
+				
 	return config
 
 
@@ -36,3 +51,19 @@ static func _decode_base64(string: String):
 static func _is_wizard_config(cfg) -> bool:
 	return cfg != null and cfg.begins_with(WIZARD_CONFIG_MARKER)
 
+static func load_config(node:Node):
+	if node.has_meta(WIZARD_CONFIG_META_NAME):
+		return node.get_meta(WIZARD_CONFIG_META_NAME)
+		
+	return decode(node.editor_description)
+	
+static func save_config(node:Node, use_metadata:bool, cfg:Dictionary):
+	if use_metadata:
+		node.set_meta(WIZARD_CONFIG_META_NAME, cfg)
+		
+		#Delete config from editor_description
+		var decoded = _decode_base64(node.editor_description)
+		if  _is_wizard_config(decoded):
+			node.editor_description = ""
+	else:
+		node.editor_description = encode(cfg)
