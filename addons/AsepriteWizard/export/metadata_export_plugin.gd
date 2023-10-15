089aef4e61b7ac2c5b14335c008ec229756b35db
Vinicius Gerevini <viniciusgerevini@gmail.com>
2023-10-15 13:24:38 -0400

    Remove noisy log from exporter plugin (#101)

--- addons/AsepriteWizard/export/metadata_export_plugin.gd
+++ addons/AsepriteWizard/export/metadata_export_plugin.gd
@@ -17,7 +17,7 @@ func _export_file(path: String, type: String, features: PoolStringArray) -> void
 		for child in node.get_children():
 			nodes.push_back(child)
 
-		if _remove_meta(node, path):
+		if _remove_meta(node):
 			scene_changed = true
 
 	#save scene if changed
@@ -28,19 +28,18 @@ func _export_file(path: String, type: String, features: PoolStringArray) -> void
 			return
 
 		var content := _get_scene_content(path, filtered_scene)
-		
+
 		add_file(path, content, true)
 
 	root_node.free()
-	
-func _remove_meta(node:Node, path: String) -> bool:
+
+func _remove_meta(node:Node) -> bool:
 	if node.has_meta(wizard_config.WIZARD_CONFIG_META_NAME):
 		node.remove_meta(wizard_config.WIZARD_CONFIG_META_NAME)
-		print("Aseprite Wizard: Removed metadata from scene %s" % path)
 		return true
-		
+
 	return false
-	
+
 func _get_scene_content(path:String, scene:PackedScene) -> PoolByteArray:
 	var tmp_path = OS.get_cache_dir()  + "tmp_scene." + path.get_extension()
 	ResourceSaver.save(tmp_path, scene)
@@ -49,7 +48,7 @@ func _get_scene_content(path:String, scene:PackedScene) -> PoolByteArray:
 	tmp_file.open(tmp_path,File.READ)
 	var content : PoolByteArray = tmp_file.get_buffer(tmp_file.get_len())
 	tmp_file.close()
-	
+
 	var dir = Directory.new()
 
 	if dir.file_exists(tmp_path):
