706c03f4c43f7998f70523f02d9acf9aaa3a821f
Vinicius Gerevini <viniciusgerevini@gmail.com>
2023-10-24 22:22:25 -0400

    cleanup

--- addons/AsepriteWizard/shared/dir_drop_button.gd
+++ addons/AsepriteWizard/shared/dir_drop_button.gd
@@ -3,9 +3,7 @@ extends Button
 
 signal dir_dropped(path)
 
-# TODO files_and_dirs
 func _can_drop_data(_pos, data):
-	print(data)
 	if data.type == "files_and_dirs":
 		var dir_access = DirAccess.open(data.files[0])
 		return dir_access != null
