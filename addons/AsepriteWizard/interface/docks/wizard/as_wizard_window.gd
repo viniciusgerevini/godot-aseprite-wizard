f319f5b896dd5b8a7698873872a057592adccf5c
Vinicius Gerevini <viniciusgerevini@gmail.com>
2024-04-05 10:01:39 -0400

    re-organize wizard dock fields

--- addons/AsepriteWizard/interface/docks/wizard/as_wizard_window.gd
+++ addons/AsepriteWizard/interface/docks/wizard/as_wizard_window.gd
@@ -15,6 +15,43 @@ var _file_dialog_aseprite: EditorFileDialog
 var _output_folder_dialog: EditorFileDialog
 var _warning_dialog: AcceptDialog
 
+@onready var _layer_section_btn: Button = $container/options/layer_section/header/section_header
+@onready var _layer_section_content: MarginContainer = $container/options/layer_section/section_content
+
+@onready var _output_section_btn: Button = $container/options/output_section/header/section_header
+@onready var _output_section_content: MarginContainer = $container/options/output_section/section_content
+
+@onready var _file_location_field: LineEdit = $container/options/file_location/HBoxContainer/file_location_path
+
+@onready var _output_folder_field: LineEdit = $container/options/output_section/section_content/items/output_folder/HBoxContainer/file_location_path
+
+@onready var _exception_pattern_field: LineEdit = $container/options/layer_section/section_content/items/exclude_pattern/pattern
+
+@onready var _split_mode_field: CheckBox = $container/options/layer_section/section_content/items/split_layers/field
+
+@onready var _only_visible_layers_field: CheckBox = $container/options/layer_section/section_content/items/visible_layers/field
+
+@onready var _custom_name_field: LineEdit = $container/options/output_section/section_content/items/custom_filename/pattern
+
+@onready var _do_not_create_res_field: CheckBox = $container/options/output_section/section_content/items/disable_resource_creation/field
+
+const INTERFACE_SECTION_KEY_LAYER = "layer_section"
+const INTERFACE_SECTION_KEY_OUTPUT = "output_section"
+
+@onready var _expandable_sections = {
+	INTERFACE_SECTION_KEY_LAYER: { "header": _layer_section_btn, "content": _layer_section_content},
+	INTERFACE_SECTION_KEY_OUTPUT: { "header": _output_section_btn, "content": _output_section_content},
+}
+
+var _interface_section_state = {}
+
+# TODO persist expanded collapsed sections
+# TODO Imported SpriteFrames tab
+
+func _ready():
+	_configure_sections()
+
+
 func _exit_tree():
 	_file_dialog_aseprite.queue_free()
 	_output_folder_dialog.queue_free()
@@ -40,36 +77,36 @@ func init(config, editor_file_system: EditorFileSystem):
 
 
 func _load_persisted_config():
-	_split_mode_field().button_pressed = _config.should_split_layers()
-	_only_visible_layers_field().button_pressed = _config.should_include_only_visible_layers()
-	_exception_pattern_field().text = _config.get_exception_pattern()
-	_custom_name_field().text = _config.get_last_custom_name()
-	_file_location_field().text = _config.get_last_source_path()
-	_do_not_create_res_field().button_pressed = _config.should_not_create_resource()
+	_split_mode_field.button_pressed = _config.should_split_layers()
+	_only_visible_layers_field.button_pressed = _config.should_include_only_visible_layers()
+	_exception_pattern_field.text = _config.get_exception_pattern()
+	_custom_name_field.text = _config.get_last_custom_name()
+	_file_location_field.text = _config.get_last_source_path()
+	_do_not_create_res_field.button_pressed = _config.should_not_create_resource()
 
 	var output_folder = _config.get_last_output_path()
-	_output_folder_field().text = output_folder if output_folder != "" else "res://"
+	_output_folder_field.text = output_folder if output_folder != "" else "res://"
 
 
 func load_import_config(import_config: Dictionary):
-	_split_mode_field().button_pressed = import_config.options.export_mode == _aseprite_file_exporter.LAYERS_EXPORT_MODE
-	_only_visible_layers_field().button_pressed = import_config.options.only_visible_layers
-	_exception_pattern_field().text = import_config.options.exception_pattern
-	_custom_name_field().text = import_config.options.output_filename
-	_file_location_field().text = import_config.source_file
-	_do_not_create_res_field().button_pressed = import_config.options.do_not_create_resource
-	_output_folder_field().text = import_config.output_location if import_config.output_location != "" else "res://"
+	_split_mode_field.button_pressed = import_config.options.export_mode == _aseprite_file_exporter.LAYERS_EXPORT_MODE
+	_only_visible_layers_field.button_pressed = import_config.options.only_visible_layers
+	_exception_pattern_field.text = import_config.options.exception_pattern
+	_custom_name_field.text = import_config.options.output_filename
+	_file_location_field.text = import_config.source_file
+	_do_not_create_res_field.button_pressed = import_config.options.do_not_create_resource
+	_output_folder_field.text = import_config.output_location if import_config.output_location != "" else "res://"
 
 
 func _open_aseprite_file_selection_dialog():
-	var current_selection = _file_location_field().text
+	var current_selection = _file_location_field.text
 	if current_selection != "":
 		_file_dialog_aseprite.current_dir = current_selection.get_base_dir()
 	_file_dialog_aseprite.popup_centered_ratio()
 
 
 func _open_output_folder_selection_dialog():
-	var current_selection = _output_folder_field().text
+	var current_selection = _output_folder_field.text
 	if current_selection != "":
 		_output_folder_dialog.current_dir = current_selection
 	_output_folder_dialog.popup_centered_ratio()
@@ -94,27 +131,27 @@ func _create_outuput_folder_selection():
 
 func _on_aseprite_file_selected(path):
 	var localized_path = ProjectSettings.localize_path(path)
-	_file_location_field().text = localized_path
+	_file_location_field.text = localized_path
 	_config.set_last_source_path(localized_path)
 
 
 func _on_output_folder_selected(path):
-	_output_folder_field().text = path
+	_output_folder_field.text = path
 	_config.set_last_output_path(path)
 
 
 func _on_next_btn_up():
-	var aseprite_file = _file_location_field().text
-	var output_location = _output_folder_field().text
-	var split_layers = _split_mode_field().button_pressed
+	var aseprite_file = _file_location_field.text
+	var output_location = _output_folder_field.text
+	var split_layers = _split_mode_field.button_pressed
 
 	var export_mode = _aseprite_file_exporter.LAYERS_EXPORT_MODE if split_layers else _aseprite_file_exporter.FILE_EXPORT_MODE
 	var options = {
 		"export_mode": export_mode,
-		"exception_pattern": _exception_pattern_field().text,
-		"only_visible_layers": _only_visible_layers_field().button_pressed,
-		"output_filename": _custom_name_field().text,
-		"do_not_create_resource": _do_not_create_res_field().button_pressed,
+		"exception_pattern": _exception_pattern_field.text,
+		"only_visible_layers": _only_visible_layers_field.button_pressed,
+		"output_filename": _custom_name_field.text,
+		"do_not_create_resource": _do_not_create_res_field.button_pressed,
 		"output_folder": output_location,
 	}
 
@@ -165,11 +202,11 @@ func _close_window():
 
 
 func _save_config():
-	_config.set_split_layers(_split_mode_field().button_pressed)
-	_config.set_exception_pattern(_exception_pattern_field().text)
-	_config.set_custom_name(_custom_name_field().text)
-	_config.set_include_only_visible_layers(_only_visible_layers_field().button_pressed)
-	_config.set_do_not_create_resource(_do_not_create_res_field().button_pressed)
+	_config.set_split_layers(_split_mode_field.button_pressed)
+	_config.set_exception_pattern(_exception_pattern_field.text)
+	_config.set_custom_name(_custom_name_field.text)
+	_config.set_include_only_visible_layers(_only_visible_layers_field.button_pressed)
+	_config.set_do_not_create_resource(_do_not_create_res_field.button_pressed)
 
 
 func _show_error(code: int):
@@ -186,37 +223,41 @@ func _show_import_success_message():
 	_warning_dialog.popup_centered()
 	_save_config()
 
-
-func _file_location_field() -> LineEdit:
-	return $container/options/file_location/HBoxContainer/file_location_path as LineEdit
-
-
-func _output_folder_field() -> LineEdit:
-	return $container/options/output_folder/HBoxContainer/file_location_path as LineEdit
-
-
-func _exception_pattern_field() -> LineEdit:
-	return $container/options/exclude_pattern/pattern as LineEdit
-
-
-func _split_mode_field() -> CheckBox:
-	return $container/options/layer_importing_mode/split_layers/field as CheckBox
-
-
-func _only_visible_layers_field() -> CheckBox:
-	return $container/options/layer_importing_mode/visible_layers/field as CheckBox
-
-
-func _custom_name_field() -> LineEdit:
-	return $container/options/custom_filename/pattern as LineEdit
-
-
-func _do_not_create_res_field() -> CheckBox:
-	return $container/options/layer_importing_mode/disable_resource_creation/field as CheckBox
-
-
 func _remove_source_files(source_files: Array):
 	for s in source_files:
 		DirAccess.remove_absolute(s.data_file)
 
 	_file_system.call_deferred("scan")
+
+
+func _configure_sections():
+	#var cfg = wizard_config.load_interface_config(target_node)
+	#_interface_section_state = cfg
+
+	for key in _expandable_sections:
+		_adjust_section_visibility(key)
+
+func _adjust_section_visibility(key: String) -> void:
+	var section = _expandable_sections[key]
+	var is_visible = _interface_section_state.get(key, true)
+	_adjust_icon(section.header, is_visible)
+	section.content.visible = is_visible
+
+
+func _adjust_icon(section: Button, is_visible: bool = true) -> void:
+	var icon_name = "GuiTreeArrowDown" if is_visible else "GuiTreeArrowRight"
+	section.icon = get_theme_icon(icon_name, "EditorIcons")
+
+
+func _toggle_section_visibility(key: String) -> void:
+	_interface_section_state[key] = not _interface_section_state.get(key, true)
+	_adjust_section_visibility(key)
+	#wizard_config.save_interface_config(target_node, _interface_section_state)
+
+
+func _on_layer_section_header_button_down():
+	_toggle_section_visibility(INTERFACE_SECTION_KEY_LAYER)
+
+
+func _on_output_section_header_button_down():
+	_toggle_section_visibility(INTERFACE_SECTION_KEY_OUTPUT)
