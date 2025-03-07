17d6641ce6e4be1449dc42097fb105456f002c46
Houman Jafarnia <houman.jafarnia@gmail.com>
2025-03-07 00:11:35 -0500

    Added Aseprite Texture Custom

---
+++ addons/AsepriteWizard/importers/static_texture_import_plugin_custom.gd
@@ -0,0 +1,66 @@
+@tool
+extends "./static_texture_import_plugin_base.gd"
+
+
+##
+## Static texture importer.
+## Imports first frame from Aseprite file as texture
+##
+
+func _get_importer_name():
+	return "aseprite_wizard.plugin.static-texture-custom"
+
+
+func _get_visible_name():
+	return "Aseprite Texture Custom"
+
+
+func _get_recognized_extensions():
+	return ["aseprite", "ase"]
+
+
+func _get_priority():
+	return 2.0 if config.get_default_importer() == config.IMPORTER_STATIC_TEXTURE_NAME else 0.8
+
+
+func _get_import_options(_path, _i):
+	return [
+		{"name": "layer/exclude_layers_pattern", "default_value": config.get_default_exclusion_pattern()},
+		{"name": "layer/only_visible_layers",    "default_value": false},
+		{
+			"name": "sheet/sheet_type",
+			"default_value": "packed",
+			"property_hint": PROPERTY_HINT_ENUM,
+			"hint_string": "columns,horizontal,vertical,packed",
+		},
+		{
+			"name": "sheet/sheet_columns",
+			"default_value": 12,
+		},
+		{"name": "animation/round_fps", "default_value": true},
+	]
+
+
+func _import(source_file, save_path, options, platform_variants, gen_files):
+	var absolute_source_file = ProjectSettings.globalize_path(source_file)
+	var source_path = source_file.get_base_dir()
+
+	var aseprite_opts = {
+		"exception_pattern": options['layer/exclude_layers_pattern'],
+		"only_visible_layers": options['layer/only_visible_layers'],
+		"output_filename": '',
+		"output_folder": source_path,
+		"sheet_type": options["sheet/sheet_type"],
+		"sheet_columns": options["sheet/sheet_columns"],
+	}
+
+	var result = _generate_texture(absolute_source_file, aseprite_opts)
+
+	if not result.is_ok:
+		printerr("ERROR - Could not import aseprite file: %s" % result_codes.get_error_message(result.code))
+		return FAILED
+
+	var sprite_sheet = result.content.sprite_sheet
+	var data = result.content.data
+
+	return _save_resource(sprite_sheet, save_path, result.content.data_file, data.meta.size)
--- addons/AsepriteWizard/plugin.gd
+++ addons/AsepriteWizard/plugin.gd
@@ -7,6 +7,7 @@ const SpriteFramesImportPlugin = preload("importers/sprite_frames_import_plugin.
 const SpriteFramesSplitImportPlugin = preload("res://addons/AsepriteWizard/importers/sprite_frames_multiple_import_plugin.gd")
 const TilesetTextureImportPlugin = preload("importers/tileset_texture_import_plugin.gd")
 const TextureImportPlugin = preload("importers/static_texture_import_plugin.gd")
+const TextureImportPluginCustom = preload("importers/static_texture_import_plugin_custom.gd")
 const TextureSplitImportPlugin = preload("res://addons/AsepriteWizard/importers/static_texture_multiple_import_plugin.gd")
 const LayerSpriteFramesImportPlugin = preload("res://addons/AsepriteWizard/importers/split_layer_importers/layer_import_plugin.gd")
 const LayerTextureImportPlugin = preload("res://addons/AsepriteWizard/importers/split_layer_importers/layer_texture_import_plugin.gd")
@@ -90,6 +91,7 @@ func _setup_importer():
 		SpriteFramesSplitImportPlugin.new(file_system_helper),
 		TilesetTextureImportPlugin.new(),
 		TextureImportPlugin.new(),
+		TextureImportPluginCustom.new(),
 		TextureSplitImportPlugin.new(file_system_helper),
 		LayerSpriteFramesImportPlugin.new(),
 		LayerTextureImportPlugin.new(),
