e05e32e7cb3b15e54d589cea3c79d29362474451
Russell Matney <russell.matney@gmail.com>
2025-08-29 14:59:19 -0400

    fix: tab/space consistency and correct var name
    Corrects an unparsable script - hopefully the rest of this logic
    actually works.

--- addons/AsepriteWizard/importers/multiple_import_plugin_base.gd
+++ addons/AsepriteWizard/importers/multiple_import_plugin_base.gd
@@ -58,8 +58,8 @@ func _import(source_file, save_path, options, platform_variants, gen_files):
 	var layers_resources_folder = options["output/layers_resources_folder"]
 
 	if layers_resources_folder.is_relative_path():
-	    var source_base_dir = source_file.get_base_dir()
-	    layer_resources_folder = source_base_dir.path_join(layers_resources_folder).simplify_path()
+		var source_base_dir = source_file.get_base_dir()
+		layers_resources_folder = source_base_dir.path_join(layers_resources_folder).simplify_path()
 
 	var import_options = _get_base_import_options(options)
 	import_options["source"] = source_file
